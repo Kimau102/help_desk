208481bcd6011c60c996f974822d2aa27955d311
Kim Au <kimau24222@gmail.com>
2023-10-15 01:34:26 +0800

    backend server rewrite async await instead of callback

--- backend/routes/ticketsRoutes.ts
+++ backend/routes/ticketsRoutes.ts
@@ -7,46 +7,31 @@ export const ticketsRoutes = express.Router()
 
 ticketsRoutes.get('/', getAllTickets)
 
-function getAllTickets(req: Request, res: Response) {
-	pool.getConnection((err, connection) => {
-		if (err) {
-			console.error('Error getting database connection:', err)
-			return res.status(500).json({ error: 'Database error' })
-		}
-		 
-		connection.query(
+async function getAllTickets(req: Request, res: Response) {
+	try {
+		const [result] = await pool.query<RowDataPacket[]>(
 			`SELECT
-              t.id,
-              CONCAT(u.first_name, ' ', u.last_name) AS requester,
-              t.modules,
-              t.subject,
-              t.cs,
-              t.priority,
-              t.status,
-              t.last_message
-            FROM
-              tickets t
-            INNER JOIN
-              users u
-            ON
-              t.requester_id = u.id;`,
-			(err, results: RowDataPacket[]) => {
-				connection.release()
-
-				if (err) {
-					console.error('Error querying MySQL:', err)
-					return res.status(500).json({ error: 'Database error' })
-				}
-
-				const formattedRes = results.map((result) => ({
-					...result,
-					last_message: formatDistanceToNow(result.last_message, { addSuffix: true }),
-				}));
-
-				res.json(formattedRes);
-				return undefined
-			}
-		)
-		return undefined
-	})
+		              t.id,
+		              CONCAT(u.first_name, ' ', u.last_name) AS requester,
+		              t.modules,
+		              t.subject,
+		              t.cs,
+		              t.priority,
+		              t.status,
+		              t.last_message
+		            FROM
+		              tickets t
+		            INNER JOIN
+		              users u
+		            ON
+		              t.requester_id = u.id;`)
+		const formattedRes = result.map((result) => ({
+			...result,
+			last_message: formatDistanceToNow(result.last_message, { addSuffix: true }),
+		}));
+		res.json(formattedRes);
+	} catch (err) {
+		console.error('Error querying MySQL:', err)
+		res.status(500).json({ error: 'Database error' })
+	}
 }
--- backend/util.ts
+++ backend/util.ts
@@ -3,10 +3,10 @@ import mysql from 'mysql2'
 
 dotenv.config()
 
-export const pool = mysql.createPool({
+export const pool = (mysql.createPool({
 	host: process.env.DB_LOACALHOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE,
 	connectionLimit: 10
-})
+})).promise()
